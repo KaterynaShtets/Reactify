748ef3f6141484ccb28cb4fb54fb525f76e54851
Katrinn23 <40269942+Katrinn23@users.noreply.github.com>
2020-11-05 16:15:54 +0200

    add proj

---
+++ JWTAuthentication/Authentication/Customer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JWTAuthentication.Authentication
+{
+    public class Customer: IdentityUser
+    {
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Shared/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Response
+    {
+    }
+}
